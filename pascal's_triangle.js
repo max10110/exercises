6f07d83c4d715dceefcc9432627a3f0607dfa8eb
max10110 <verovenkomax@gmail.com>
2020-02-27 20:53:40 +0000

    Pascal's Triangle add

---
+++ pascal's_triangle.js
@@ -0,0 +1,7 @@
+function pasc(row, element){
+    if(row == 1 || element == 1 || row == element){
+        return 1;
+    }else{
+        return pasc(row - 1, element - 1) + pasc(row - 1, element);
+    }
+}
